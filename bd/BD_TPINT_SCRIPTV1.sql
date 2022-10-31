29e7a62dd04e7995bd786a4dcb853748ae2a49e5
facundo-piana-sampietro <80989927+facundo-piana-sampietro@users.noreply.github.com>
2022-10-30 21:38:04 -0300

    Actualización de la BD

--- bd/BD_TPINT_SCRIPTV1.sql
+++ bd/BD_TPINT_SCRIPTV1.sql
@@ -4,35 +4,36 @@ create schema bd_tpint_labiv;
 
 use bd_tpint_labiv;
 
+
 CREATE TABLE Paises
 (
-	Id_Pais int auto_increment NOT NULL,
+	IdPais_Pais int auto_increment NOT NULL,
 	Descripcion_Pais VARCHAR(30) NOT NULL,
 
-	CONSTRAINT PK_PAISES PRIMARY KEY (Id_Pais)
+	CONSTRAINT PK_PAISES PRIMARY KEY (IdPais_Pais)
 );
 
 
 CREATE TABLE Provincias
 (
-	Id_Pais_Provincia int NOT NULL,
-	Id_Provincia int auto_increment NOT NULL,
-	Descripcion_Provincia VARCHAR(30) NOT NULL,
+	IdPais_Prov int NOT NULL,
+	IdProvincia_Prov int auto_increment NOT NULL,
+	Descripcion_Prov VARCHAR(30) NOT NULL,
 
-	CONSTRAINT PK_PROVINCIAS PRIMARY KEY (Id_Provincia, Id_Pais_Provincia),
-    CONSTRAINT FK_PROVINCIAS_PAISES FOREIGN KEY (Id_Pais_Provincia)
-		REFERENCES Paises(Id_Pais)
+	CONSTRAINT PK_PROVINCIAS PRIMARY KEY (IdProvincia_Prov, IdPais_Prov),
+    CONSTRAINT FK_PROVINCIAS_PAISES FOREIGN KEY (IdPais_Prov)
+		REFERENCES Paises(IdPais_Pais)
 );
 
 CREATE TABLE Localidades(
-	Id_Pais_Localidad int NOT NULL,
-	Id_Provincia_Localidad int NOT NULL,
-    Id_Localidad int auto_increment NOT NULL,
-	Descripcion_Localidad VARCHAR(30) NOT NULL,
+	IdPais_Loc int NOT NULL,
+	IdProvincia_Loc int NOT NULL,
+    IdLocalidad_Loc int auto_increment NOT NULL,
+	Descripcion_Loc VARCHAR(30) NOT NULL,
 
-	CONSTRAINT PK_Localidades PRIMARY KEY (Id_Localidad, Id_Provincia_Localidad, Id_Pais_Localidad),
-    CONSTRAINT FK_LOCALIDADES_PROVINCIAS FOREIGN KEY (Id_Pais_Localidad, Id_Provincia_Localidad)
-		REFERENCES Provincias(Id_Pais_Provincia ,Id_Provincia)
+	CONSTRAINT PK_Localidades PRIMARY KEY (IdLocalidad_Loc, IdProvincia_Loc, IdPais_Loc),
+    CONSTRAINT FK_LOCALIDADES_PROVINCIAS FOREIGN KEY (IdPais_Loc, IdProvincia_Loc)
+		REFERENCES Provincias(IdPais_Prov, IdProvincia_Prov)
 );
 
 CREATE TABLE Usuarios(
@@ -40,87 +41,87 @@ CREATE TABLE Usuarios(
     CUIL_Usr CHAR(12) UNIQUE NOT NULL,
 	Nombre_Usr VARCHAR(20) NOT NULL,
 	Apellido_Usr VARCHAR(20),
-    Genero_Usr VARCHAR(10) check(Genero_Usuario like 'MASCULINO' OR Genero_Usuario like 'FEMENINO' OR Genero_Usuario like 'INDEFINIDO'),
+    Genero_Usr VARCHAR(10) check(Genero_Usr like 'MASCULINO' OR Genero_Usr like 'FEMENINO' OR Genero_Usr like 'INDEFINIDO'),
 	Nacionalidad VARCHAR(30) NOT NULL,
     FechaNacimiento_Usr DATE NULL,
     Direccion_Usr VARCHAR(30) NOT NULL,
-    Id_Pais_Usr INT NOT NULL,
-    Id_Provincia_Usr INT NOT NULL,
-    Id_Localidad_Usr INT NOT NULL,
-    Telefono_Usuario VARCHAR(10) NULL,
+    IdPais_Usr INT NOT NULL,
+    IdProvincia_Usr INT NOT NULL,
+    IdLocalidad_Usr INT NOT NULL,
+    Telefono_Usr VARCHAR(10) NULL,
 	Tipo_Usr BIT DEFAULT 0, -- 0 normal, 1 admin
 	Contrasenia_Usr VARCHAR(16) NOT NULL, 
-	Estado_Usuario BIT DEFAULT 1,
+	Estado_Usr BIT DEFAULT 1,
     
 	CONSTRAINT PK_USUARIOS PRIMARY KEY(DNI_Usr),
-	CONSTRAINT FK_LOCALIDADES_USUARIOS FOREIGN KEY (Id_Localidad_Usr, Id_Provincia_Usr, Id_Pais_Usr)
-		REFERENCES Localidades(id_Localidad, Id_Provincia_Localidad, Id_Pais_Localidad)
+	CONSTRAINT FK_LOCALIDADES_USUARIOS FOREIGN KEY (IdLocalidad_Usr, IdProvincia_Usr, IdPais_Usr)
+		REFERENCES Localidades(IdLocalidad_Loc, IdProvincia_Loc, IdPais_Loc)
 );
 
 CREATE TABLE TipoCuentas(
-	id_TipoCuenta INT auto_increment NOT NULL,
-    descripcion_TipoCuenta VARCHAR(20),
+	Id_TipoCuenta INT auto_increment NOT NULL,
+    Descripcion_TipoCuenta VARCHAR(20),
     
-    CONSTRAINT PK_TIPO_CUENTAS PRIMARY KEY(id_TipoCuenta)
+    CONSTRAINT PK_TIPO_CUENTAS PRIMARY KEY(Id_TipoCuenta)
 );
 
 
 CREATE TABLE Cuentas(
-	Nro_cuenta VARCHAR(5) NOT NULL,
-    DNI_cuenta CHAR(10) NOT NULL,
-    fecha_creacion_cuenta TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    id_TipoCuenta_Cuentas INT NOT NULL,
+	Nro_Cuentas VARCHAR(5) NOT NULL,
+    DNI_Cuentas CHAR(10) NOT NULL,
+    FechaCreacion_Cuentas TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    IdTipoCuenta_Cuentas INT NOT NULL,
     CBU VARCHAR(16) NOT NULL UNIQUE,
-    Saldo_cuenta DECIMAL NOT NULL DEFAULT 10000.0,
-    Estado_cuenta BIT DEFAULT 1,
+    Saldo_Cuentas DECIMAL NOT NULL DEFAULT 10000.0,
+    Estado_Cuentas BIT DEFAULT 1,
     
-    CONSTRAINT PK_CUENTAS PRIMARY KEY(Nro_cuenta),
-	CONSTRAINT FK_CUENTAS_USUARIOS FOREIGN KEY (DNI_cuenta)
-		REFERENCES usuarios(DNI_Usr),
-	CONSTRAINT FK_CUENTAS_TIPOCUENTAS FOREIGN KEY (id_TipoCuenta_Cuentas)
-		REFERENCES TipoCuentas(id_TipoCuenta)
+    CONSTRAINT PK_CUENTAS PRIMARY KEY(Nro_Cuentas),
+	CONSTRAINT FK_CUENTAS_USUARIOS FOREIGN KEY (DNI_Cuentas)
+		REFERENCES Usuarios(DNI_Usr),
+	CONSTRAINT FK_CUENTAS_TIPOCUENTAS FOREIGN KEY (IdTipoCuenta_Cuentas)
+		REFERENCES TipoCuentas(Id_TipoCuenta)
 );
 
 CREATE TABLE Prestamos(
-	id_prestamo INT auto_increment NOT NULL,
-    DNI_prestamo CHAR(10) NOT NULL,
-    nro_cuentaDestino_prestamo VARCHAR(5) NOT NULL,
-    Autorizado_prestamo BIT NOT NULL DEFAULT 0 , -- 1 AUTORIZADO, 0 NO AUTORIZADO
-    fecha_solicitado_prestamo TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    imp_solicitado_prestamo DECIMAL NOT NULL,
-    imp_resultante_prestamo DECIMAL NOT NULL,
-    plazo_meses_prestamo INT NOT NULL,
-    imp_pagoAlMes_prestamo DECIMAL NOT NULL,
-    cant_cuotas_prestamo INT NOT NULL,
+	Id_Pr INT auto_increment NOT NULL,
+    DNI_Pr CHAR(10) NOT NULL,
+    NroCuentaDestino_Pr VARCHAR(5) NOT NULL,
+    Autorizado_Pr BIT NOT NULL DEFAULT 0 , -- 1 AUTORIZADO, 0 NO AUTORIZADO
+    FechaSolicitado_Pr TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    ImpSolicitado_Pr DECIMAL NOT NULL,
+    ImpResultante_Pr DECIMAL NOT NULL,
+    PlazoMeses_Pr INT NOT NULL,
+    ImpPagoAlMes_Pr DECIMAL NOT NULL,
+    CantCuotas_Pr INT NOT NULL,
     
-    CONSTRAINT PK_PRESTAMOS PRIMARY KEY(id_prestamo),
-    CONSTRAINT FK_USUARIOS_PRESTAMOS FOREIGN KEY (DNI_prestamo)
-		REFERENCES usuarios(DNI_Usr),
-	CONSTRAINT FK_CUENTAS_PRESTAMOS FOREIGN KEY (nro_cuentaDestino_prestamo)
-		REFERENCES cuentas(Nro_cuenta)
+    CONSTRAINT PK_PRESTAMOS PRIMARY KEY(Id_Pr),
+    CONSTRAINT FK_USUARIOS_PRESTAMOS FOREIGN KEY (DNI_Pr)
+		REFERENCES Usuarios(DNI_Usr),
+	CONSTRAINT FK_CUENTAS_PRESTAMOS FOREIGN KEY (NroCuentaDestino_Pr)
+		REFERENCES Cuentas(Nro_Cuentas)
         
 );
 
 CREATE TABLE TiposMovimientos(
-	id_tiposMovimientos INT auto_increment NOT NULL,
-    descripcion_tiposMovimientos VARCHAR(30) NOT NULL,
+	Id_TiposMov INT auto_increment NOT NULL,
+    Descripcion_TiposMov VARCHAR(30) NOT NULL,
 	
-    CONSTRAINT PK_TiposMovimientos PRIMARY KEY(id_tiposMovimientos)
+    CONSTRAINT PK_TiposMovimientos PRIMARY KEY(Id_TiposMov)
 );
 
 CREATE TABLE Movimientos(
-	id_movimiento INT auto_increment NOT NULL,
-    nro_cuenta_movimientos VARCHAR(5) NOT NULL,
-    id_tiposMovimientos_movimientos INT NOT NULL,
-    fecha_movimientos TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    detalle_movimientos VARCHAR(30) NOT NULL,
-    importe_movimientos DECIMAL NOT NULL,
+	Id_Mov INT auto_increment NOT NULL,
+    NroCuenta_Mov VARCHAR(5) NOT NULL,
+    IdTiposMov_Mov INT NOT NULL,
+    Fecha_Mov TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    Detalle_Mov VARCHAR(30) NOT NULL,
+    Importe_Mov DECIMAL NOT NULL,
     
-    CONSTRAINT PK_Movimientos PRIMARY KEY(id_movimiento),
-    CONSTRAINT FK_MOVIMIENTOS_CUENTAS FOREIGN KEY (nro_cuenta_movimientos)
-		REFERENCES cuentas(Nro_cuenta),
-	CONSTRAINT FK_MOVIMIENTOS_TIPOS FOREIGN KEY (id_tiposMovimientos_movimientos)
-		REFERENCES TiposMovimientos(id_tiposMovimientos)
+    CONSTRAINT PK_Movimientos PRIMARY KEY(Id_Mov),
+    CONSTRAINT FK_MOVIMIENTOS_CUENTAS FOREIGN KEY (NroCuenta_Mov)
+		REFERENCES Cuentas(Nro_Cuentas),
+	CONSTRAINT FK_MOVIMIENTOS_TIPOS FOREIGN KEY (IdTiposMov_Mov)
+		REFERENCES TiposMovimientos(Id_TiposMov)
 );
 
 
