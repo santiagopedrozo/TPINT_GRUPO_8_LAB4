e4875cdb90536deb161a47653dbc5c1de9096315
facundo-piana-sampietro <80989927+facundo-piana-sampietro@users.noreply.github.com>
2022-11-06 13:05:00 -0300

    .

--- WebContent/IniciarSesion.jsp
+++ WebContent/IniciarSesion.jsp
@@ -9,6 +9,7 @@
 <body>
 <H1> <b>  <p style="text-align:center;">Bienvenido al banco Brusafa, tu banco de confianza! </b></H1>
 <form method="get" action="Inicio.jsp">
+<<<<<<< Updated upstream
 <p style="text-align:center;"> Ingrese sus credenciales para poder ingresar
 <br> <br>
 Usuario:  <input type="text" placeholder="Ingrese usuario" name="txtUsuario" />
@@ -17,6 +18,54 @@ Contraseña:  <input type="password" placeholder="Ingrese contraseña" name= "txtC
 <br> <br>
 <input type="submit" name= "btnIniciar" value="Iniciar sesión"/>  
 </p>
+=======
+
+
+<div style="width: 40%; margin: auto">
+      <div class="card text-center">
+        <div class="card-header">Iniciar sesión</div>
+        <div class="card-body">
+          <form method="get" action="ServletUsuarios.jsp" class="row">
+            <div class="col-md-12">
+              <div class="form-floating mb-3">
+                <input
+                  type="text"
+                  class="form-control"
+                  id="floatingInput"
+                  name="txtUsuario"
+                  placeholder="-"
+                />
+                <label for="floatingInput">Usuario</label>
+              </div>
+            </div>
+
+            <div class="col-md-12">
+              <div class="form-floating mb-3">
+                <input
+                  type="password"
+                  class="form-control"
+                  id="floatingInput"
+                  name= "txtContraseña"
+                  placeholder="-"
+                />
+                <label for="floatingInput">Contraseña</label>
+              </div>
+            </div>
+
+            <div class="col-md-12">
+              <button
+                type="submit"
+                class="btn btn-outline-primary form-control btn-lg"
+                name="btnIniciar"
+              >
+                Ingresar
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+>>>>>>> Stashed changes
 </form>
 </body>
 </html>
---
+++ src/presentacion/controlador/servletUsuarios.java
@@ -0,0 +1,115 @@
+package presentacion.controlador;
+
+import java.io.IOException;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import daoImpl.UsuariosDaoImpl;
+import entidades.Localidades;
+import entidades.Provincias;
+import entidades.Usuarios;
+import negocio.LocalidadesNegocio;
+import negocio.ProvinciasNegocio;
+import negocio.UsuariosNegocio;
+import negocioImpl.LocalidadesNegocioImpl;
+import negocioImpl.ProvinciasNegocioImpl;
+import negocioImpl.UsuariosNegocioImpl;
+
+@WebServlet("/servletUsuarios")
+public class servletUsuarios extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private ProvinciasNegocio provNeg = new ProvinciasNegocioImpl();
+	private LocalidadesNegocio locNeg = new LocalidadesNegocioImpl();
+	private UsuariosNegocio usrNeg= new UsuariosNegocioImpl();
+
+    public servletUsuarios() {
+    
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		refreshDeGet(request);
+
+		RequestDispatcher rd = request.getRequestDispatcher("AdministrarUsuarios.jsp");
+		rd.forward(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if(request.getParameter("provSeleccionada") != null && request.getParameter("btnAgregarUsr") == null) //cuando sean muchos estaria bueno que sea un switch de parametros
+			filtradoLocalidades(request);	
+		if (request.getParameter("btnAgregarUsr")!=null) {
+			asignarInputsAEntidad(request);
+		} 
+		
+		if (request.getParameter("btnIniciar")!=null) {
+			iniciarSesion(request);
+		}
+		
+		
+		refreshDePost(request);
+		RequestDispatcher rd = request.getRequestDispatcher("AdministrarUsuarios.jsp");
+		rd.forward(request, response);
+	}
+	
+	private void refreshDeGet(HttpServletRequest request) {
+		cargarProvinciasUser(request);
+		cargarLocalidadesUser(request, -1);
+	}
+	
+	private void refreshDePost(HttpServletRequest request){
+		cargarProvinciasUser(request);
+	}
+	
+	private void filtradoLocalidades(HttpServletRequest request) {
+		int idProv = Integer.parseInt(request.getParameter("ddlProvincias").toString());
+		cargarLocalidadesUser(request, idProv);
+	}
+	
+	
+	
+	private void cargarProvinciasUser(HttpServletRequest request) {		
+		request.setAttribute("provincias", provNeg.readALL());
+	}
+	
+	private void cargarLocalidadesUser(HttpServletRequest request, int idProv) {
+		request.setAttribute("localidades", locNeg.GetAllLocalidadesPorProv(idProv));
+	}
+	
+	private void asignarInputsAEntidad(HttpServletRequest request) {
+		boolean agrego=false;
+		System.out.println("adentro de asignarInputs");
+		String DNI_Usr = request.getParameter("txtDNI");
+		String CUIL_Usr = request.getParameter("txtCUIL");
+		String Nombre_Usr = request.getParameter("txtNombre");
+		String Apellido_Usr = request.getParameter("txtApellido");
+		String Sexo_Usr = request.getParameter("txtSexo");
+		String Nacionalidad_Usr  = request.getParameter("txtNacionalidad");
+		System.out.println("antes de parsear fecha" + LocalDate.parse(request.getParameter("Fecha")));
+		LocalDate FechaNacimiento_Usr = LocalDate.parse(request.getParameter("Fecha"));
+		System.out.println("fecha parseada: " + FechaNacimiento_Usr.toString());
+		String Direccion_Usr= request.getParameter("txtDireccion");
+		Provincias Provincia_Usr = new Provincias(Integer.parseInt(request.getParameter("ddlProvincias")),null);  
+		Localidades Localidad_Usr = new Localidades(Provincia_Usr,locNeg.buscarNumLoc(request.getParameter("ddlLocalidades")),null);
+		String Email_Usr = request.getParameter("txtEmail");
+		String Telefono_Usr = request.getParameter("txtTelefono");
+		boolean Tipo_Usr = Boolean.parseBoolean(request.getParameter("ddlTipo"));
+		String Usuario_Usr = request.getParameter("txtUsuario");
+		String Contrasenia_Usr = request.getParameter("txtContrasenia");
+		Usuarios user = new Usuarios (DNI_Usr, CUIL_Usr,Nombre_Usr,  Apellido_Usr, Sexo_Usr, Nacionalidad_Usr, FechaNacimiento_Usr, Direccion_Usr,Provincia_Usr, Localidad_Usr ,Email_Usr,Telefono_Usr,  Tipo_Usr, Usuario_Usr,  Contrasenia_Usr, true);
+		//otra funcion aca
+		agrego=usrNeg.insert(user);
+		System.out.println(agrego);
+	}
+	
+	private void iniciarSesion(HttpServletRequest request) {
+		
+	}
+	
+}
